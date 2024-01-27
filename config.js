e7fd007d0cf586b23ac3d7034df3c67e5a294550
YodaImperium <philipp.bruhns@gmx.de>
2024-01-27 21:06:46 +0100

    test

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-let calendar = '';dfkjdfk
-let socials = '';
+let calendar = '<iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2hDOH4UTXyHlub8cC-O3rtwHHVm3fDDF_3i8NFyfbXKdK_Ywx6gXpQRtjgx9eaL8OjGRlRtirI?gv=true" style="border: 0" width="100%" height="600" frameborder="0"></iframe>';
+let socials = '<a href="https://instagram.com/flippoo1"><box-icon type="logo" name="instagram" size="lg" animation="tada-hover"></box-icon></a><a href="https://github.com/YodaImperium/schlaumai.de"><box-icon name="github" type="logo" size="lg" animation="tada-hover"></box-icon></a>';
 
 document.querySelector("#booking").innerHTML = calendar;
 document.querySelector("#social").innerHTML = socials;
